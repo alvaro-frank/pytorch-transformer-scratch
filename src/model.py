94bffe63a440b2fcd6247fa31bf5ec9ee2091389
alvaro-frank <alvarojf96@hotmail.com>
2026-02-10 19:19:02 +0000

    feat: Add BigramLanguageModel class

---
+++ src/model.py
@@ -0,0 +1,20 @@
+import torch
+import torch.nn as nn
+from torch.nn import functional as F
+
+class BigramLanguageModel(nn.Module):
+    def __init__(self, vocab_size):
+        super().__init__()
+        self.token_embedding_table = nn.Embedding(vocab_size, vocab_size)
+
+    def forward(self, idx, targets=None):
+        logits = self.token_embedding_table(idx)
+
+        if targets is None:
+            loss = None
+        else:
+            B, T, C = logits.shape
+            
+            loss = F.cross_entropy(logits.view(B * T, C), targets.view(B * T))
+
+        return logits, loss
